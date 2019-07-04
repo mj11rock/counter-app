90412de74f2e8d81487f0449c98eaea950434530
mj11rock <mj11rock@gmail.com>
2019-07-04 22:47:08 +0500

    Updates

---
+++ precache-manifest.f8ef0811808151bb2fd141304ceec1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4e93de5c6f2af81e8fb918ee58c1d4",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "b2d3ed89bd029342b000",
+    "url": "/counter-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "991b054305812c67c3d6",
+    "url": "/counter-app/static/css/main.a7617a66.chunk.css"
+  },
+  {
+    "revision": "b2d3ed89bd029342b000",
+    "url": "/counter-app/static/js/2.eb088328.chunk.js"
+  },
+  {
+    "revision": "991b054305812c67c3d6",
+    "url": "/counter-app/static/js/main.13513cbd.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  }
+]);
